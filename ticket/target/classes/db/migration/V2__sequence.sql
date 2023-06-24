d0cced03bef305720a0365ac07367a9e20d9da34
Ivanna Ksiondzyk <112642424+ksoiva@users.noreply.github.com>
2023-06-24 12:31:19 +0300

    Delete ticket/target/classes directory

--- ticket/target/classes/db/migration/V2__sequence.sql
+++
@@ -1,5 +0,0 @@
-CREATE SEQUENCE hibernate_sequence START 100;
-ALTER TABLE "user" ALTER COLUMN id SET DEFAULT nextval('hibernate_sequence');
-ALTER TABLE "flight" ALTER COLUMN id SET DEFAULT nextval('hibernate_sequence');
-ALTER TABLE "ticket" ALTER COLUMN id SET DEFAULT nextval('hibernate_sequence');
-ALTER TABLE "plane" ALTER COLUMN id SET DEFAULT nextval('hibernate_sequence');
